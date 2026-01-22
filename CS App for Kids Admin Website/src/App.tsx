5e92d528dace69abc11ce09f8ac30f9cf6312e77
ekacala <elizabethkacala@gmail.com>
2026-01-22 17:16:37 -0600

    Setup pages and created basic layouts for each page

--- CS App for Kids Admin Website/src/App.tsx
+++ CS App for Kids Admin Website/src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css' 
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Welcome to Citizen Science App for Kids</h1>
+      <div id='login'>
+        <form>
+          <label form='username'>Username: </label>
+          <input type='text' id='username' name='username'></input><br/>
+          <label form='password'>Password: </label>
+          <input type='text' id='password' name='password'></input><br/>
+          <input type='button' value={'submit'} id='submit' name='submit'></input>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+function ProjectList() {
+  return (
+    <>
+    <h1>Projects</h1>
+    <div id='project-list'>
+      <table id='project-list-table'>
+        <tr>
+          <td>Title</td>
+          <td>Description</td>
+          <td>Code</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+    </>
+  )
+}
+
+function NewProject() {
+  return (
+    <>
+    <h1>Create a Project</h1>
+    <div id='create-project'>
+      <button>X</button>
+      <form id='create-project-form'>
+        <label form='title'>Title: </label>
+        <input type='text' id='title' name='title'></input><br/>
+        <label form='description'>Description: </label>
+        <input type='text' id='description' name='description'></input><br/>
+        <label form='instructions'>Instructions: </label>
+        <input type='text' id='instructions' name='instructions'></input><br/>
+        <label form='observations'>Observations: </label>
+        <input type='text' id='observations' name='observations'></input><br/>
+        <input type='button' value={'submit'} id='submit' name='submit'></input>
+      </form>
+    </div>
+    </>
+  )
+}
+
+function ProjectResults() {
+  return (
+    <>
+    <h1>Project Results</h1>
+    <button>Back</button>
+    <div id='project-results'>
+      <table id='project-results-table'>
+        <tr>
+          <td>Student</td>
+          <td>Observation</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+      <button>Create Me a Graph</button>
+      <button>Download SVG</button>
+    </div>
+    </>
+  )
+}
+
+export {App, ProjectList, NewProject, ProjectResults};
--- CS App for Kids Admin Website/src/main.tsx
+++ CS App for Kids Admin Website/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import {App, ProjectList, NewProject, ProjectResults} from './App.tsx' 
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+
+      <Route path='projects' element={<ProjectList />} />
+
+      <Route path='new-project' element={<NewProject />} />
+
+      <Route path='project-results' element={<ProjectResults />} />
+    </Routes>
+  </BrowserRouter>
 )
