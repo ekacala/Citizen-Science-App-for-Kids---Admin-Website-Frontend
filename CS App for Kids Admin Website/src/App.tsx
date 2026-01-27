6bf7177920c87b5d409d29a745c4d280a0d6153d
ekacala <elizabethkacala@gmail.com>
2026-01-27 15:03:25 -0600

    Changed issues for linter

--- CS App for Kids Admin Website/src/App.tsx
+++ CS App for Kids Admin Website/src/App.tsx
@@ -29,18 +29,18 @@ function Login() {
 
 /*
  * Create form to request access token from Google's OAuth 2.0 server. - Plan to set this up next sprint
- */
+ 
 function oauthSignIn() {
   // Google's OAuth 2.0 endpoint for requesting an access token
-  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+  const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
   // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-  var form = document.createElement('form');
+  const form = document.createElement('form');
   form.setAttribute('method', 'GET'); // Send as a GET request.
   form.setAttribute('action', oauth2Endpoint);
 
   // Parameters to pass to OAuth 2.0 endpoint.
-  var params = {'client_id': 'YOUR_CLIENT_ID',
+  const params = {'client_id': 'YOUR_CLIENT_ID',
                 'redirect_uri': 'YOUR_REDIRECT_URI',
                 'response_type': 'token',
                 'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/calendar.readonly',
@@ -48,8 +48,8 @@ function oauthSignIn() {
                 'state': 'pass-through value'};
 
   // Add form parameters as hidden input values.
-  for (var p in params) {
-    var input = document.createElement('input');
+  for (let p in params) {
+    const input = document.createElement('input');
     input.setAttribute('type', 'hidden');
     input.setAttribute('name', p);
     input.setAttribute('value', p);
@@ -59,7 +59,7 @@ function oauthSignIn() {
   // Add form to page and submit it to open the OAuth 2.0 endpoint.
   document.body.appendChild(form);
   form.submit();
-}
+}*/
 
 function ProjectList() {
   /* Returns the html for the project list page. */
