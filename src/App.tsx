ef262ecfe573090ae3f5e7b11d91f3dd9837eb58
ekacala <elizabethkacala@gmail.com>
2026-02-14 10:49:20 -0600

    Fixed linting error and fixed creating field of type text bug

--- src/App.tsx
+++ src/App.tsx
@@ -178,7 +178,7 @@ function NewProject() {
   const [projectFieldLabel, setProjectFieldLabel] = useState('')
   //const [projectFieldOptions, setProjectFieldOptions] = useState('')
   const [projectIsRequired, setProjectIsRequired] = useState('false')
-  let boolProjectIsRequired = false
+  const [boolProjectIsRequired, setBoolProjectIsRequired] = useState(false)
 
   // Track progress through form
   const [projectCreated, setProjectCreated] = useState(false)
@@ -190,7 +190,7 @@ function NewProject() {
 
     // Convert projectIsRequired to boolean
     if (projectIsRequired == 'true') {
-      boolProjectIsRequired = true
+      setBoolProjectIsRequired(true)
     }
 
     // Check if form input is empty
@@ -233,9 +233,10 @@ function NewProject() {
   useEffect(() => {
     if(dataAdded) {
       setProjectFieldName('')
-      setProjectFieldType('')
+      setProjectFieldType('text')
       setProjectFieldLabel('')
       setProjectIsRequired('false')
+      setBoolProjectIsRequired(false)
       setDataAdded(false)
     }
   }, [addData])
